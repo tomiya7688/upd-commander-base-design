44fa51e42668ba5301d0a5de729a7816082861f3
kadoka <tomiya3499@gmail.com>
2026-09-13 02:43:10 +0900

    Add Go checker models

---
+++ support_tools/go/upd_commander_checker/internal/checker/models.go
@@ -0,0 +1,16 @@
+package checker
+
+type Finding struct {
+	Path     string
+	Line     int
+	Code     string
+	Message  string
+	Severity string
+}
+
+type ModuleInfo struct {
+	Path          string
+	Layer         string
+	Role          string
+	ApplicationID string
+}
