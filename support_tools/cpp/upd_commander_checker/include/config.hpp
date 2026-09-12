4225f0654c8615620ed18091e19be1032c1f7c18
kadoka <tomiya3499@gmail.com>
2026-09-13 02:54:27 +0900

    Add C++ checker config model

---
+++ support_tools/cpp/upd_commander_checker/include/config.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace upd_checker {
+
+struct Config {
+    std::string input = ".";
+    std::string output;
+    std::vector<std::string> ignore;
+    bool warnings_as_errors = false;
+};
+
+Config load_config(const std::string& executable_path);
+
+}  // namespace upd_checker
