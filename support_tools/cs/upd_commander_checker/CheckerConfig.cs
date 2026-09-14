649728b41df0c161573a100710903a746c179272
kadoka <tomiya3499@gmail.com>
2026-09-14 18:19:39 +0900

    Split C# checker config model for responsibility compliance

---
+++ support_tools/cs/upd_commander_checker/CheckerConfig.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace UpdCommanderChecker;
+
+internal sealed class CheckerConfig
+{
+    [JsonPropertyName("input")]
+    public string Input { get; set; } = ".";
+
+    [JsonPropertyName("output")]
+    public string Output { get; set; } = "";
+
+    [JsonPropertyName("ignore")]
+    public List<string> Ignore { get; set; } = new();
+
+    [JsonPropertyName("warnings_as_errors")]
+    public bool WarningsAsErrors { get; set; }
+}
