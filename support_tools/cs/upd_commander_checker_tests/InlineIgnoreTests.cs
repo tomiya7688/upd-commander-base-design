a237d3b5f527f2cf88d94d3463dda6d79441ce98
kadoka <tomiya3499@gmail.com>
2026-09-14 20:10:08 +0900

    Add CSharp inline ignore regression test

---
+++ support_tools/cs/upd_commander_checker_tests/InlineIgnoreTests.cs
@@ -0,0 +1,14 @@
+using UpdCommanderChecker;
+
+namespace UpdCommanderChecker.Tests;
+
+public sealed class InlineIgnoreTests
+{
+    [Fact]
+    public void InlineIgnoreSuppressesRule()
+    {
+        using var project = new TempProject();
+        project.Write("process/inline_commander.cs", "namespace Sample; internal sealed class InlineCommander { internal int Run() => 1 + 2; // upd: ignore UPD202 - test\n}");
+        Assert.DoesNotContain(project.Scan(), item => item.Code == "UPD202");
+    }
+}
