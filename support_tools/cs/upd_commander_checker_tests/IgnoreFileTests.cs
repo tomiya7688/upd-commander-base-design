adb329b4f09025d755b59b09b84cc119e4c95147
kadoka <tomiya3499@gmail.com>
2026-09-14 20:10:17 +0900

    Add CSharp ignore file regression test

---
+++ support_tools/cs/upd_commander_checker_tests/IgnoreFileTests.cs
@@ -0,0 +1,15 @@
+using UpdCommanderChecker;
+
+namespace UpdCommanderChecker.Tests;
+
+public sealed class IgnoreFileTests
+{
+    [Fact]
+    public void IgnoreFileSuppressesSpecificRule()
+    {
+        using var project = new TempProject();
+        project.Write("process/rule_commander.cs", "namespace Sample; internal sealed class RuleCommander { internal int Run() => 1 + 2; }");
+        project.Write(".updcommanderignore", "UPD202 process/rule_commander.cs # test\n");
+        Assert.DoesNotContain(project.Scan(), item => item.Code == "UPD202");
+    }
+}
