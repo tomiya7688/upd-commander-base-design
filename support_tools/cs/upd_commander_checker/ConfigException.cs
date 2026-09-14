34f082ab737166cc7db764e5fbffbd44cc2ff540
kadoka <tomiya3499@gmail.com>
2026-09-14 19:11:34 +0900

    Make ConfigException single-input

--- support_tools/cs/upd_commander_checker/ConfigException.cs
+++ support_tools/cs/upd_commander_checker/ConfigException.cs
@@ -2,8 +2,8 @@ namespace UpdCommanderChecker;
 
 internal sealed class ConfigException : Exception
 {
-    internal ConfigException(string message, Exception? innerException = null)
-        : base(message, innerException)
+    internal ConfigException(string message)
+        : base(message)
     {
     }
 }
