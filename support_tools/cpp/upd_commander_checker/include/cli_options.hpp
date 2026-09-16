2ae329bad9e98a385befab7a7e86eeb92c878d77
kadoka <tomiya3499@gmail.com>
2026-09-16 12:37:10 +0900

    Keep one C++ CLI data type per header

--- support_tools/cpp/upd_commander_checker/include/cli_options.hpp
+++ support_tools/cpp/upd_commander_checker/include/cli_options.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <stdexcept>
 #include <string>
 #include <vector>
 
+#include "cli_usage_error.hpp"
 #include "config.hpp"
 
 namespace upd_checker {
@@ -14,11 +14,6 @@ struct CliOptions {
     std::vector<std::string> ignores;
     bool warnings_as_errors = false;
     bool attentions_as_errors = false;
-};
-
-class CliUsageError : public std::runtime_error {
-public:
-    using std::runtime_error::runtime_error;
 };
 
 CliOptions parse_cli(int argc, char* argv[], const Config& config);
