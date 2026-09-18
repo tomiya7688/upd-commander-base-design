e84df9da4b910a6e27474eeceff6e1ac8db6c86f
kadoka <tomiya3499@gmail.com>
2026-09-19 01:13:44 +0900

    Add Go ranged integer config validation

--- support_tools/go/upd_commander_checker/internal/checker/config_fields.go
+++ support_tools/go/upd_commander_checker/internal/checker/config_fields.go
@@ -19,6 +19,18 @@ func decodeConfigBool(value json.RawMessage, target *bool) bool {
 	return json.Unmarshal(value, target) == nil
 }
 
+func decodeConfigIntRange(value json.RawMessage, target *int, minimum int, maximum int) bool {
+	if isJSONNull(value) {
+		return false
+	}
+	var decoded int
+	if json.Unmarshal(value, &decoded) != nil || decoded < minimum || decoded > maximum {
+		return false
+	}
+	*target = decoded
+	return true
+}
+
 func decodeConfigPositiveInt(value json.RawMessage, target *int) bool {
 	if isJSONNull(value) {
 		return false
