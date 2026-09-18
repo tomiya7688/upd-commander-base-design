13b24c4cd73a83301d86755c1345749ba2fa0137
kadoka <tomiya3499@gmail.com>
2026-09-19 00:49:31 +0900

    Format C# UPD301 test helper

--- support_tools/cs/upd_commander_checker_tests/TempProject.cs
+++ support_tools/cs/upd_commander_checker_tests/TempProject.cs
@@ -38,10 +38,7 @@ internal sealed class TempProject : IDisposable
         File.WriteAllText(path, content);
     }
 
-    internal List<Finding> Scan(
-        IReadOnlyList<string>? ignore = null,
-        int upd301MaxInputs = 2
-    )
+    internal List<Finding> Scan(IReadOnlyList<string>? ignore = null, int upd301MaxInputs = 2)
     {
         return Scanner.ScanPath(new ScanPathInput(root, ignore ?? [], upd301MaxInputs));
     }
