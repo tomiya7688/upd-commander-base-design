541789d77b2ebf4d9640589d1855975561e46f49
kadoka <tomiya3499@gmail.com>
2026-09-19 01:14:26 +0900

    Add C# UPD405 scan thresholds

--- support_tools/cs/upd_commander_checker/ScanPathInput.cs
+++ support_tools/cs/upd_commander_checker/ScanPathInput.cs
@@ -3,5 +3,7 @@ namespace UpdCommanderChecker;
 internal sealed record ScanPathInput(
     string Target,
     IReadOnlyList<string> CliIgnore,
-    int Upd301MaxInputs = 2
+    int Upd301MaxInputs = 2,
+    int FlatLayerMinFiles = 12,
+    int FlatLayerMinDirectPercent = 80
 );
