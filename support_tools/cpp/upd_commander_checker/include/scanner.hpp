fad264132b70a8e303047dcb20446d6ec9d7b60e
kadoka <tomiya3499@gmail.com>
2026-09-18 12:41:51 +0900

    Add C++ scan threshold parameter

--- support_tools/cpp/upd_commander_checker/include/scanner.hpp
+++ support_tools/cpp/upd_commander_checker/include/scanner.hpp
@@ -9,6 +9,7 @@ namespace upd_checker {
 
 std::vector<Finding> scan_path(
     const std::string& target,
-    const std::vector<std::string>& cli_ignore);
+    const std::vector<std::string>& cli_ignore,
+    int upd301_max_inputs = 2);
 
 }  // namespace upd_checker
