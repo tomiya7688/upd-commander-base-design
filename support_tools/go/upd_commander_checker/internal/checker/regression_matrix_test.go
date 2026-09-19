c4f558a3b4f24dddea488e685586f7973fe33a6c
kadoka <tomiya3499@gmail.com>
2026-09-19 12:03:05 +0900

    Use Go checker assertion input container

--- support_tools/go/upd_commander_checker/internal/checker/regression_matrix_test.go
+++ support_tools/go/upd_commander_checker/internal/checker/regression_matrix_test.go
@@ -10,9 +10,9 @@ func TestCommanderRulesMatrix(t *testing.T) {
 	path := filepath.Join(root, "process", "rule_commander.go")
 	writeTestFile(t, path, "package process\nimport \"os\"\nfunc run() { for i := 0; i < 2; i++ { _ = i + 1 }; _, _ = os.ReadFile(\"sample.txt\") }\n")
 	findings := ScanPath(root, nil)
-	assertHasCode(t, findings, "UPD201")
-	assertHasCode(t, findings, "UPD202")
-	assertHasCode(t, findings, "UPD203")
+	assertHasCode(codeAssertionInput{t: t, findings: findings, code: "UPD201"})
+	assertHasCode(codeAssertionInput{t: t, findings: findings, code: "UPD202"})
+	assertHasCode(codeAssertionInput{t: t, findings: findings, code: "UPD203"})
 }
 
 func TestCommanderComparisonsDoNotTriggerUPD202(t *testing.T) {
@@ -20,7 +20,7 @@ func TestCommanderComparisonsDoNotTriggerUPD202(t *testing.T) {
 	path := filepath.Join(root, "process", "compare_commander.go")
 	writeTestFile(t, path, "package process\nfunc run(left, right int, ready, enabled bool) bool { return left == right || left != right || left < right || left > right || (ready && enabled) }\n")
 	findings := ScanPath(root, nil)
-	assertNoCode(t, findings, "UPD202")
+	assertNoCode(codeAssertionInput{t: t, findings: findings, code: "UPD202"})
 }
 
 func TestCommanderArithmeticOperatorsTriggerUPD202(t *testing.T) {
@@ -31,7 +31,7 @@ func TestCommanderArithmeticOperatorsTriggerUPD202(t *testing.T) {
 			path := filepath.Join(root, "process", "math_commander.go")
 			writeTestFile(t, path, "package process\nfunc run(left, right int) int { return left "+operator+" right }\n")
 			findings := ScanPath(root, nil)
-			assertHasCode(t, findings, "UPD202")
+			assertHasCode(codeAssertionInput{t: t, findings: findings, code: "UPD202"})
 		})
 	}
 }
@@ -41,9 +41,9 @@ func TestContainerRulesMatrix(t *testing.T) {
 	path := filepath.Join(root, "process", "large_commander.go")
 	writeTestFile(t, path, "package process\ntype LargeCommander struct{}\nfunc (LargeCommander) Run(\n a int,\n b int,\n c int,\n d int,\n e int,\n f int,\n g int,\n h int,\n i int,\n j int,\n k int,\n l int,\n) (int, int) { return a, b }\n")
 	findings := ScanPath(root, nil)
-	assertHasCode(t, findings, "UPD301")
-	assertHasCode(t, findings, "UPD302")
-	assertHasCode(t, findings, "UPD303")
+	assertHasCode(codeAssertionInput{t: t, findings: findings, code: "UPD301"})
+	assertHasCode(codeAssertionInput{t: t, findings: findings, code: "UPD302"})
+	assertHasCode(codeAssertionInput{t: t, findings: findings, code: "UPD303"})
 }
 
 func TestUnexportedReceiverMethodIsChecked(t *testing.T) {
@@ -51,8 +51,8 @@ func TestUnexportedReceiverMethodIsChecked(t *testing.T) {
 	path := filepath.Join(root, "process", "private_processing.go")
 	writeTestFile(t, path, "package process\ntype worker struct{}\nfunc (worker) run(left, right, mode int) (int, int) { return left, right }\n")
 	findings := ScanPath(root, nil)
-	assertHasCode(t, findings, "UPD301")
-	assertHasCode(t, findings, "UPD302")
+	assertHasCode(codeAssertionInput{t: t, findings: findings, code: "UPD301"})
+	assertHasCode(codeAssertionInput{t: t, findings: findings, code: "UPD302"})
 }
 
 func TestConstructorLikePackageFunctionIsNotChecked(t *testing.T) {
@@ -60,7 +60,7 @@ func TestConstructorLikePackageFunctionIsNotChecked(t *testing.T) {
 	path := filepath.Join(root, "process", "constructor_processing.go")
 	writeTestFile(t, path, "package process\ntype worker struct{}\nfunc NewWorker(left, right int) worker { return worker{} }\n")
 	findings := ScanPath(root, nil)
-	assertNoCode(t, findings, "UPD301")
+	assertNoCode(codeAssertionInput{t: t, findings: findings, code: "UPD301"})
 }
 
 func TestCliPathIgnoreMatrix(t *testing.T) {
@@ -68,5 +68,5 @@ func TestCliPathIgnoreMatrix(t *testing.T) {
 	path := filepath.Join(root, "process", "ignored_commander.go")
 	writeTestFile(t, path, "package process\nfunc run() { _ = 1 + 2 }\n")
 	findings := ScanPath(root, []string{"process/**"})
-	assertNoCode(t, findings, "UPD202")
+	assertNoCode(codeAssertionInput{t: t, findings: findings, code: "UPD202"})
 }
