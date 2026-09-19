e6d39f5b57c8e913779299bc6c94091019da4f1c
kadoka <tomiya3499@gmail.com>
2026-09-19 12:02:43 +0900

    Containerize Go finish wrapper input

--- support_tools/go/upd_commander_checker/cmd/upd-commander-check/main.go
+++ support_tools/go/upd_commander_checker/cmd/upd-commander-check/main.go
@@ -10,6 +10,12 @@ import (
 
 type stringList []string
 
+type finishInput struct {
+	lines  []string
+	output string
+	code   int
+}
+
 func (items *stringList) String() string { return fmt.Sprint([]string(*items)) }
 func (items *stringList) Set(value string) error {
 	*items = append(*items, value)
@@ -43,7 +49,7 @@ func main() {
 	warningsAsErrors = warningsAsErrors || config.WarningsAsErrors
 
 	if _, err := os.Stat(target); err != nil {
-		finish([]string{fmt.Sprintf("E UPD000 %s missing", target)}, output, 2)
+		finish(finishInput{lines: []string{fmt.Sprintf("E UPD000 %s missing", target)}, output: output, code: 2})
 	}
 
 	findings := checker.FilterEnabledFindings(checker.ScanPathWithAllThresholds(target, ignores, config.Upd301MaxInputs, config.FlatLayerMinFiles, config.FlatLayerMinDirectPercent, config.ModelGroupMinItems, config.ModelGroupMinOccurrences), config.EnabledRules)
@@ -68,16 +74,16 @@ func main() {
 
 	if errors > 0 || warningsAsErrors && warnings > 0 || attentionsAsErrors && attentions > 0 {
 		lines = append(lines, fmt.Sprintf("FAIL e=%d w=%d a=%d", errors, warnings, attentions))
-		finish(lines, output, 1)
+		finish(finishInput{lines: lines, output: output, code: 1})
 	}
 	if warnings > 0 || attentions > 0 {
 		lines = append(lines, fmt.Sprintf("OK w=%d a=%d", warnings, attentions))
 	} else {
 		lines = append(lines, "OK")
 	}
-	finish(lines, output, 0)
+	finish(finishInput{lines: lines, output: output, code: 0})
 }
 
-func finish(lines []string, output string, code int) {
-	os.Exit(finishReport(lines, output, code))
+func finish(input finishInput) {
+	os.Exit(finishReport(input.lines, input.output, input.code))
 }
